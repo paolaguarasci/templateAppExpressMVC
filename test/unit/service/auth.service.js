7c938c4d1ad78780fb26e5f9992ac79c9753e259
Paola Guarasci <paolaguarasci@gmail.com>
2022-10-24 08:23:52 +0200

    auth service test

--- test/unit/service/auth.service.js
+++ test/unit/service/auth.service.js
@@ -1,13 +1,17 @@
 // deepcode ignore NoHardcodedPasswords/test: <please specify a reason of ignoring this>
 import AuthService from "../../../src/service/auth.service.js";
 
-
 jest.mock("../../../src/model/UserBase.js", () => ({
   findOne: jest.fn().mockImplementation((criteria) => {
+    if (criteria.username !== "ciccio") {
+      throw Error("User not found");
+    }
     return {
       username: criteria.username,
       role: "user",
-      comparePassword: jest.fn().mockResolvedValue(true),
+      comparePassword: jest.fn().mockImplementation((pass) => {
+        return pass === "pasticcio";
+      }),
     };
   }),
 }));
@@ -30,8 +34,24 @@ describe("Test Auth Service", function () {
     expect(savedUserOnDB.username).toBe("ciccio");
   });
 
-  test("Should receive user user with same ID as asked", async () => {
+  test("Should receive user user with same username as asked", async () => {
     let savedUserOnDB = await AuthService.login("ciccio", "pasticcio");
     expect(savedUserOnDB.username).toBe("ciccio");
+  });
+
+  test("Should trowh an empty {} if try to login with wrong password", async () => {
+    const l = await AuthService.login("ciccio", "ciccio");
+    expect(JSON.stringify(l)).toBe("{}");
+  });
+
+  test("Should trowh an erro if try to login with wrong username", async () => {
+    let error;
+    try {
+      await AuthService.login("ciao", "ciccio");
+    } catch (e) {
+      error = e;
+    }
+    expect(error).toBeDefined();
+    expect(error.message).toBe("Error: User not found");
   });
 });
