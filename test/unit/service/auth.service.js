ad7a7d82d6db59ae32813c255459916bc86b086d
Paola Guarasci <paolaguarasci@gmail.com>
2022-10-24 07:56:05 +0200

    mock correct user model methods

--- test/unit/service/auth.service.js
+++ test/unit/service/auth.service.js
@@ -1,27 +1,20 @@
 // deepcode ignore NoHardcodedPasswords/test: <please specify a reason of ignoring this>
 import AuthService from "../../../src/service/auth.service.js";
 
-const userUsers = [
-  { id: 1, username: "john" },
-  { id: 2, username: "ted" },
-  { id: 3, username: "mark" },
-];
 
 jest.mock("../../../src/model/UserBase.js", () => ({
-  findOne: jest
-    .fn()
-    .mockImplementation((id) => userUsers.find((admin) => admin.id === id)),
+  findOne: jest.fn().mockImplementation((criteria) => {
+    return {
+      username: criteria.username,
+      role: "user",
+      comparePassword: jest.fn().mockResolvedValue(true),
+    };
+  }),
 }));
 
 jest.mock("../../../src/model/User.js", () => ({
   create: jest.fn().mockImplementation((request) => {
     return { username: request.username, password: request.password };
-  }),
-}));
-
-jest.mock("../../../src/model/UserBase.js", () => ({
-  findOne: jest.fn().mockImplementation((criteria) => {
-    return { username: criteria.username };
   }),
 }));
 
@@ -37,8 +30,8 @@ describe("Test Auth Service", function () {
     expect(savedUserOnDB.username).toBe("ciccio");
   });
 
-  // test("Should receive user user with same ID as asked", async () => {
-  //   let savedUserOnDB = await AuthService.login("ciccio", "pasticcio");
-  //   expect(savedUserOnDB.username).toBe("ciccio");
-  // });
+  test("Should receive user user with same ID as asked", async () => {
+    let savedUserOnDB = await AuthService.login("ciccio", "pasticcio");
+    expect(savedUserOnDB.username).toBe("ciccio");
+  });
 });
